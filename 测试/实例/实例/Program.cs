2febc0f04c1aaff628b45da0171eb6904b546da3
syyyhl <syyyhl@DESKTOP-ID64D6N>
2021-04-28 16:56:46 +0800

    1

---
+++ 测试/实例/实例/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 实例
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("zh-CN");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN");
+
+            Application.Run(new GridControl学习());
+        }
+    }
+}
